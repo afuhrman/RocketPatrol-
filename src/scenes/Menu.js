92f1ad5c4ba57b51de11541abe0fecc32beacd9a
afuhrman <afuhrman@ucsc.edu>
2022-04-17 15:52:10 -0700

    meaningful

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,14 +8,13 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-        this.load.audio('sfx_EpicMusic', './assets/EpicMusic.wav'); // Copyright-free music, Hostiles Inbound [Epic Battle Music – Powerful Orchestral] by Miguel Johnson (https://www.chosic.com/download-audio/29436/)
       }
 
     
     create() {
 
         let menuConfig = {
-            fontFamily: 'Fantasy',
+            fontFamily: 'Courier',
             fontSize: '28px',
             backgroundColor: '#F3B141',
             color: '#FFFFFF',
@@ -27,18 +26,15 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
           }
 
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCK PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5); 
         menuConfig.backgroundColor = '#00FF00'; 
-        menuConfig.color = '#FFFFFF'; 
+        menuConfig.color = '#000'; 
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <-- for Novice or --> for Expert', menuConfig).setOrigin(0.5);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT); 
-        var music = this.sound.add('sfx_EpicMusic'); 
-        music.setLoop(true);
-        music.play(); 
-        //code source (https://stackoverflow.com/questions/34210393/looping-audio-in-phaser)
-    }
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        } 
+
 
     update() {
       if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
